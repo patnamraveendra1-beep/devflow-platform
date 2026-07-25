ba7453a33c94cabfc36a26523e1a26bdd28357c9
patnam Raveendra <raveendra@ravi.aws>
2026-07-25 09:36:32 -0700

    Test Jenkins webhook trigger

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Welcome to DevFlow CI/CD"}
+    return {"message": "Welcome to DevFlow CI/CD success"}
 
 @app.get("/health")
 def health():
