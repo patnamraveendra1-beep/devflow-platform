a796fa5f10f6df9c5e9ce7fe6d2d5eadea9d4435
patnam Raveendra <raveendra@ravi.aws>
2026-07-25 22:30:10 -0700

    Add frontend project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div>
+      <h1>🚀 DevFlow</h1>
+      <h2>CI/CD Deployment Dashboard</h2>
+
+      <hr />
+
+      <h3>Backend Status</h3>
+      <p>✅ FastAPI Running</p>
+
+      <h3>CI/CD Pipeline</h3>
+      <p>✅ GitHub → Jenkins → Docker → AWS</p>
+
+      <h3>Infrastructure</h3>
+      <p>✅ Docker Container</p>
+      <p>✅ AWS EC2</p>
+      <p>✅ Nginx Reverse Proxy</p>
+    </div>
+  )
+}
+
+export default App
